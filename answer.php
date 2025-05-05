c093eec637a70e56ef473c7b3ddfcd72a3935203
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-05 23:57:30 +0000

    added changes to index.php & answer.php

--- answer.php
+++ answer.php
@@ -46,33 +46,20 @@
       <div class="right-image">
         <img src="./images/dice.gif" alt="dice" />
       </div>
-      <div class="page-content">Please enter a number between 1 and 6:</div>
-      <br />
-      <!-- Simple Textfield for integers-->
-      <form action="answer.php" method="GET">
-        <div class="mdl-textfield mdl-js-textfield">
-          <!-- input pattern attribute -->
-          <input
-            class="mdl-textfield__input"
-            type="text"
-            pattern="-?[0-9]*(\.[0-9]+)?"
-            name="user-number"
-            step="0.01" />
-          <!-- mdl-textfield__label -->
-          <label class="mdl-textfield__label" for="demo-input">Number...</label>
-          <!-- class "mdl-textfield__error" -->
-          <span class="mdl-textfield__error">Input is not a number</span>
-        </div>
-        <br />
-        <!-- Accent-colored raised button with ripple -->
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          type="submit">
-          Check
-        </button>
-      </form>
+      <?php
+      $randomNumber = rand(1, 6);
+      $userNumber = $_GET["user-number"];
+
+      if ($userNumber == $randomNumber) {
+        echo "<p>Correct! The number was: $randomNumber.</p>";
+      }
+      if ($userNumber != $randomNumber) {
+        echo "<p>Wrong! The number was: $randomNumber.</p>";
+      }
+      ?>
       <br />
       <div class="page-content">
-        <div id="result"></div>
+        <a href="./index.php">Return ...</a>
       </div>
     </main>
   </div>
